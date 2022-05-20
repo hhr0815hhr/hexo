9b65fe2715e6947f3917757e0a4e5d95f0542826
691129301 <691129301@qq.com>
2022-05-20 08:08:39 +0000

    up

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -14,9 +14,10 @@ namespace App\Controller;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\GetMapping;
 
-#[Controller(prefix:"index")]
+#[Controller(prefix:"/index")]
 class IndexController extends AbstractController
 {
+    #[GetMapping(path:"index")]
     public function index()
     {
         $user = $this->request->input('user', 'Hyperf');
